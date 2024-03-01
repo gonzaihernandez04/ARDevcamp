266968956e5b22a47ce9a37579f2cd9185b96798
Gonzalo <hernandezgonzalo5845@gmail.com>
2024-03-01 01:02:47 -0300

    style: se agrego barra de navegacion debajo del header. se crearon nuevas hojas de estilo en el header. BARRA Y navegacion. se estilizo todo

--- views/templates/header.php
+++ views/templates/header.php
@@ -26,3 +26,18 @@
         </div>
     </div>
 </header>
+
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="">
+           <h2 class="barra__logo"> &#60;ARGDevCamp/></h2>
+        </a>
+        <div class="navegacion">
+            <a href="/argdevcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshops-conferencias" class="navegacion__enlace">Workshops - Conferencias</a>
+            <a href="/registro" class="navegacion__enlace"> Comprar Pase</a>
+        </div>
+    </div>
+</div>
