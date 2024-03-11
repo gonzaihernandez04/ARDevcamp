eff19de013f7e46d1be97cc7c352b70fc393c51c
Gonzalo <hernandezgonzalo5845@gmail.com>
2024-03-11 18:53:54 -0300

    feat: se creo template para los eventos. Se muestran tanto los workshops como las conferencas

--- views/paginas/conferencias-workshops.php
+++ views/paginas/conferencias-workshops.php
@@ -11,48 +11,43 @@
 
                 <?php foreach ($eventos['conferencias_v'] as $evento) : ?>
 
-                    <div class="evento swiper-slide">
-                        <p class="evento__hora"><?php echo $evento->hora->hora; ?></p>
-
-                        <div class="evento__informacion">
-                            <h4 class="evento__nombre"><?php echo $evento->nombre; ?></h4>
+                    <?php include __DIR__ . '/../templates/evento.php' ?>
 
 
-                            <p class="evento__introduccion"><?php echo $evento->descripcion; ?></p>
+                <?php endforeach; ?>
 
+            </div>
 
-                            <div class="evento__autor-info">
-                                <picture>
-                                    <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.webp " type='image/webp'>
-                                    <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.webp " type='image/png'>
-                                    <img class="evento__imagen-autor" loading="lazy" width="200" height="300" src="<?php echo $_ENV['HOST'] . '/img/sepakers/' . $evento->ponente->imagen ?>.png" alt="Imagen ponente">
-                                </picture>
-
-                                <p class="evento__autor-nombre"><?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?></p>
-                            </div>
-                        </div>
-
-
-                    </div>
-        
-
-        <?php endforeach; ?>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
 
         </div>
 
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
+
+        <p class="eventos__fecha">Sabado 6 de octubre</p>
+
+
+        <div class="eventos__listado">
+
+            <div class="eventos__listado slider swiper">
+                <div class="swiper-wrapper">
+
+                    <?php foreach ($eventos['conferencias_s'] as $evento) : ?>
+
+                        <?php include __DIR__ . '/../templates/evento.php' ?>
+
+
+                    <?php endforeach; ?>
+
+                </div>
+
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+
+            </div>
+        </div>
 
     </div>
-
-
-    <p class="eventos__fecha">Sabado 6 de octubre</p>
-
-    <div class="eventos__listado">
-    </div>
-    </div>
-
-
 
 
     <div class="eventos eventos--workshops ">
@@ -60,21 +55,47 @@
         <p class="eventos__fecha">Viernes 5 de octubre</p>
 
         <div class="eventos__listado">
+            <div class="eventos__listado slider swiper">
+                <div class="swiper-wrapper">
 
+                    <?php foreach ($eventos['workshops_v'] as $evento) : ?>
+
+                        <?php include __DIR__ . '/../templates/evento.php' ?>
+
+
+                    <?php endforeach; ?>
+
+                </div>
+
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+
+            </div>
         </div>
 
 
         <p class="eventos__fecha">Sabado 6 de octubre</p>
 
         <div class="eventos__listado">
+            <div class="eventos__listado slider swiper">
+                <div class="swiper-wrapper">
 
+                    <?php foreach ($eventos['workshops_s'] as $evento) : ?>
+
+                        <?php include __DIR__ . '/../templates/evento.php' ?>
+
+
+                    <?php endforeach; ?>
+
+                </div>
+
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+
+            </div>
         </div>
     </div>
 </main>
 
 
 <script src="/build/js/main.js" defer></script>
-
-
-
-
